0127ecd0e6174cb0fdacae248e63a611a48dfe15
Sergey <shrokov.37@gmail.com>
2024-12-04 11:42:50 +0300

    v1.0.2 - mod f

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { createRoot } from 'react-dom/client'
-import store from "./store";
+import { store } from "./store/store"
 import { GamePage } from "./pages/GamePage";
 
 createRoot(document.getElementById('root')).render(
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,11 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import apiReducer from './apiSlice'
 import gameReducer from './gameSlice'
 
-const store = configureStore({
+export const store = configureStore({
     reducer: {
         games: apiReducer,
         game: gameReducer
     },
 })
-
-export default store;
